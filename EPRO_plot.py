e65aba7dc534e3fd1e97df6308011969333a6d86
SIAR1992 <SIAR1992@users.noreply.github.com>
2020-03-06 08:52:49 +0100

    Tilføjelse af vector_func
    Til forkortning af kode har jeg tilføjet vector_func funktionen. Den skal gøre den oprindelige kode mere overskuelig (Dvs. at vi gemmer lang kode inde i andre funktioner). Kig gerne på dem, og se om det kan optimeres

--- EPRO_plot.py
+++ EPRO_plot.py
@@ -13,6 +13,7 @@ import numpy as np #se overstående
 import matplotlib.pyplot as plt #Er Pythons plotter modul, som indeholder en masse funktioner til at plotte med
 #----------------------------------OBS!!!!---------------------------------------------------------------------
 import farver_func as ffc
+import vector_func as vcf
 #--------------------------------------------------------------------------------------------------------------
 import math #Standard Python modul med matematik funktioner hentes ind her
 import os #OS modulet hjælper Python med funktioner til at arbejde rundt i Operative System (Computeren)
@@ -42,6 +43,8 @@ for f in var: #For-loop igennem alle valgte filer
 #Udtagning og behandling af data fra Excel------------------------------------------------------------
 data=[pd.read_excel(path) for path in filesNames] #Panda funktion, som henter data ind fra præfabrikerede Excel-filer 
 Common_names =["X", "Varmeforbrug", "Samlet behov", "Lagertab", "Behov alene"] #Her skrives der nogle "Almindelig" navne, det forventes at EnergyPro bruger, det er til fra sortering af Data, således at man kun får det mest nødvendige - Det kan være man er nødsagt til at kigge gennem Excel-filen her
+
+
 data_reduced = data #Opretter en ny dataframe til at bearvejde data, uden at noget går tabt - Det kan være at denne skal fjernes på et senere tidspunkt
  
 allLabels = [] #Opretter list til at hente alle labels brugt i Excel, samt at koordinere farvevalg derimellem.      
@@ -49,32 +52,37 @@ allLabels = [] #Opretter list til at hente alle labels brugt i Excel, samt at ko
 #Specifik lagring af data til brug i plots senere
 
 for i in range(len(filesNames)): #For loop for behandling af data, der loopes igennem antallet af filer valgt
-    data[i] = data[i].apply(lambda x: pd.Series(x.dropna().values)) #Panda funktion, der tager alle NaN fundet i Excel-filen, det kan have noget at gøre med hvordan data ligger i Excel-filen
-    data[i] = data[i].dropna(axis=1, how='all') #Tager alle NaN-værdier i søjler og fjerner dem, skulle det være rækker skulle man skrive axis=0
+   globals()['m%s' % i], globals()['label%s' % i], globals()['VF%s' % i], allLabels = vcf.vector_func(i,filesNames, data, data_reduced, Common_names, allLabels)
+    #-------------------------- FJERNES MED NY FUNKTION ------------------------------------------
+#    data[i] = data[i].apply(lambda x: pd.Series(x.dropna().values)) #Panda funktion, der tager alle NaN fundet i Excel-filen, det kan have noget at gøre med hvordan data ligger i Excel-filen
+#    data[i] = data[i].dropna(axis=1, how='all') #Tager alle NaN-værdier i søjler og fjerner dem, skulle det være rækker skulle man skrive axis=0
 
-    globals()['dm%s' % i] = data[i].values #Omdanner dataframen til en matrix, hvorved andre funktioner kan benyttes (Bemærk Global funktionen, der opretter nye navne for dm, således det bliver dm0, dm1 osv alt efter antaller af valgte filer)
-    globals()['names%s' % i] = globals()['dm%s' % i][0,:] #Henter navne, som bliver brugt til senere labeling af plot mm.
-    data[i].columns = globals()['names%s' % i] #Omdanner navne i dataframe "data", som bruges til først at sætte det samlede varmeforbrug for analysens plot - Det bruges til at navigere i dataframen imellem navne såsom "Varmetab" mm.
-    if "Lagertab" in globals()['names%s' % i]: #Hvis der er lagertab (Antaget lager med tab i EPRO) - kan det være at varmeforbruget i analysen er defineret som "Varmeforbrug" - OBS! - vær opmærksom på at EnergyPro skifter navne for flere forskellige analyser, har ikke selv gennemskuet fidusen endnu
-        globals()['dm_varmeforbrug%s' % i] = data[i]["Varmeforbrug"].values #Antager at der er noget, der hedder "Varmeforbrug", foruddybning se overstående
-    else: 
-        globals()['dm_varmeforbrug%s' % i] = data[i].T.iloc[-1].values #Hvis der ikke er noget "Lagertab" så antages der at varmebehovet er samlet i sidste kolonne i Excel
-    
-    globals()['VF%s' % i] = globals()['dm_varmeforbrug%s' % i][1:globals()['dm_varmeforbrug%s' % i].shape[0]].tolist()  #Der sorteres, således at kun værdierne fremstår i vektorer, og omdannes med nympy, således at det kan bruges i plot senere
-    removers = list(set(Common_names).intersection(set(globals()['names%s' % i].tolist()))) #Danner en liste med ord, der skal fjernes jf. "Common_names"
-    for String in removers: #Gennemgår "removers" for at fjerne irrellevant data fra behandling
-        data_reduced[i] = data_reduced[i].drop([String],axis = 1) #Her fjernes unødvendigt data igennem "String"
+#    globals()['dm%s' % i] = data[i].values #Omdanner dataframen til en matrix, hvorved andre funktioner kan benyttes (Bemærk Global funktionen, der opretter nye navne for dm, således det bliver dm0, dm1 osv alt efter antaller af valgte filer)
+#    globals()['names%s' % i] = globals()['dm%s' % i][0,:] #Henter navne, som bliver brugt til senere labeling af plot mm.
+#    data[i].columns = globals()['names%s' % i] #Omdanner navne i dataframe "data", som bruges til først at sætte det samlede varmeforbrug for analysens plot - Det bruges til at navigere i dataframen imellem navne såsom "Varmetab" mm.
+#    if "Lagertab" in globals()['names%s' % i]: #Hvis der er lagertab (Antaget lager med tab i EPRO) - kan det være at varmeforbruget i analysen er defineret som "Varmeforbrug" - OBS! - vær opmærksom på at EnergyPro skifter navne for flere forskellige analyser, har ikke selv gennemskuet fidusen endnu
+#        globals()['dm_varmeforbrug%s' % i] = data[i]["Varmeforbrug"].values #Antager at der er noget, der hedder "Varmeforbrug", foruddybning se overstående
+#    else: 
+#        globals()['dm_varmeforbrug%s' % i] = data[i].T.iloc[-1].values #Hvis der ikke er noget "Lagertab" så antages der at varmebehovet er samlet i sidste kolonne i Excel
+#    
+#    globals()['VF%s' % i] = globals()['dm_varmeforbrug%s' % i][1:globals()['dm_varmeforbrug%s' % i].shape[0]].tolist()  #Der sorteres, således at kun værdierne fremstår i vektorer, og omdannes med nympy, således at det kan bruges i plot senere
+#    removers = list(set(Common_names).intersection(set(globals()['names%s' % i].tolist()))) #Danner en liste med ord, der skal fjernes jf. "Common_names"
+#    for String in removers: #Gennemgår "removers" for at fjerne irrellevant data fra behandling
+#        data_reduced[i] = data_reduced[i].drop([String],axis = 1) #Her fjernes unødvendigt data igennem "String"
             
-    globals()['dm_reduced%s' % i] = data_reduced[i].values #omformulerer "data_reduced" til noget der kan arbejdes med, tænker at denne kan komprimeres lidt
-    globals()['Values%s' % i] = globals()['dm_reduced%s' % i][1:globals()['dm%s' % i].shape[0],1:globals()['dm%s' % i].shape[1]] #Tager kun værdierne ud, da disse skal bruges til plottene - fjerner indeks
+#    globals()['dm_reduced%s' % i] = data_reduced[i].values #omformulerer "data_reduced" til noget der kan arbejdes med, tænker at denne kan komprimeres lidt
+#    globals()['Values%s' % i] = globals()['dm_reduced%s' % i][1:globals()['dm%s' % i].shape[0],1:globals()['dm%s' % i].shape[1]] #Tager kun værdierne ud, da disse skal bruges til plottene - fjerner indeks
     
-    globals()['label%s' % i] = globals()['dm_reduced%s' % i][0,1:globals()['dm_reduced%s' % i].shape[1]] #Udtager navne, denne er en der kan tages
-    allLabels.extend(globals()['label%s' % i].tolist()) #Alle labels der bruges i alle hentede Excel-filer bliver lagret i denne, hvilket skal bruges til farvelægningen
+#    globals()['label%s' % i] = globals()['dm_reduced%s' % i][0,1:globals()['dm_reduced%s' % i].shape[1]] #Udtager navne, denne er en der kan tages
+#    allLabels.extend(globals()['label%s' % i].tolist()) #Alle labels der bruges i alle hentede Excel-filer bliver lagret i denne, hvilket skal bruges til farvelægningen
     
-    globals()['m%s' % i] = [] #Opretter matrix til at indeholde værdier der skal bruges til stackplot senere, bliver nød til, foreløbigt at gøre det på denne måde, da Pythons .tolist()-function opstiller matrix forkert smides den på med det samme
-    for k in range(0,globals()['Values%s' % i].shape[1]): #For loop over størrelsen af "Values"- Denne kan nok komprimeres lidt
-            globals()['m%s' % i].append(globals()['Values%s' % i][:,k].tolist()) #Omdanner til værdier der kan bruges i plot (på bestemt måde) og samler disse i matrix
-            
+#    globals()['m%s' % i] = [] #Opretter matrix til at indeholde værdier der skal bruges til stackplot senere, bliver nød til, foreløbigt at gøre det på denne måde, da Pythons .tolist()-function opstiller matrix forkert smides den på med det samme
+#    for k in range(0,globals()['Values%s' % i].shape[1]): #For loop over størrelsen af "Values"- Denne kan nok komprimeres lidt
+#            globals()['m%s' % i].append(globals()['Values%s' % i][:,k].tolist()) #Omdanner til værdier der kan bruges i plot (på bestemt måde) og samler disse i matrix
+
+    
+    
+#---------------------------------------------------------------------------------------------            
 #-----------------------------------------------------------------------------------------------------
 
 #-------------- Farver -----------------------------------------------
@@ -84,10 +92,11 @@ canvas1 = tk.Canvas(root, width = 300, height = 300) #Informationer der skal bru
 canvas1.pack() #funktion der skal bruges til at lave vinduet for spørgsmålet
 farverBrugt, allLabelUser = ffc.farver_func(allLabels)
 
-for i in range(len(filesNames)): #Danner for loop til at smide farvene ind for hver Excel-fil
-    globals()['colorsForYou%s' % i] = [] #Laver parameter til gemme farver for hver Excel-fil
-    for k in range(len(globals()['label%s' % i])): #Kører igennem længden af "Labels", da farvekoderne skal matches med deres pågældende label
-        globals()['colorsForYou%s' % i].append(farverBrugt[allLabelUser.index(globals()['label%s' % i][k])]) #Her matches farvekoder med deres pågældende label og farve kode og gemmes i den nye parameter "colorsForYou"
+if None not in farverBrugt:
+    for i in range(len(filesNames)): #Danner for loop til at smide farvene ind for hver Excel-fil
+        globals()['colorsForYou%s' % i] = [] #Laver parameter til gemme farver for hver Excel-fil
+        for k in range(len(globals()['label%s' % i])): #Kører igennem længden af "Labels", da farvekoderne skal matches med deres pågældende label
+            globals()['colorsForYou%s' % i].append(farverBrugt[allLabelUser.index(globals()['label%s' % i][k])]) #Her matches farvekoder med deres pågældende label og farve kode og gemmes i den nye parameter "colorsForYou"
         
 #---------------------------------------------------------------------
 
